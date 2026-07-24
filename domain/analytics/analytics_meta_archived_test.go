9582c8cdf659cb5b3c577fdc182b151f5adc3e62
Tudor Hulban <prindex2010@gmail.com>
2026-07-24 14:42:56 +0300

    fix TestMetaActive_ZeroValueKey

--- domain/analytics/analytics_meta_archived_test.go
+++ domain/analytics/analytics_meta_archived_test.go
@@ -7,45 +7,65 @@ import (
 )
 
 func TestMetaActive_ZeroValueKey(t *testing.T) {
-	var m MetaActive[string]
+	var m1 MetaActive[string]
 
 	// 1. Insert a valid empty-string key
-	m.Increment("", 1)
+	m1.Increment("", 1)
 
 	// Expect slot 0 to contain the empty string
-	require.Equal(t, "", *m.Names[0].Load())
-	require.Equal(t, uint32(1), m.Values[0].Load())
+	require.Equal(t,
+		"",
+		*m1.Names[0].Load(),
+	)
+	require.Equal(t,
+		uint32(1),
+		m1.Values[0].Load(),
+	)
 
 	// 2. Insert another key
-	m.Increment("A", 1)
+	m1.Increment("A", 1)
 
 	// Correct behavior: "" must remain untouched.
 	require.Equal(t,
 		"",
-		*m.Names[0].Load(),
+		*m1.Names[0].Load(),
 
 		"empty-string key was incorrectly treated as empty slot",
 	)
-	require.Equal(t, uint32(1), m.Values[0].Load())
+	require.Equal(t,
+		uint32(1),
+		m1.Values[0].Load(),
+	)
 
-	// // 3. Merge with another MetaActive also containing ""
-	// var m2 MetaActive[string]
-	// m2.Increment("", 2)
+	// 3. Merge with another MetaActive also containing ""
+	var m2 MetaActive[string]
 
-	// merged := MetaActive[string]{}.MergeActive(&m)
-	// merged = merged.MergeActive(&m2)
+	m2.Increment("", 2)
 
-	// // Correct behavior: "" must be included and summed.
-	// require.Equal(t,
-	// 	"",
-	// 	merged.Names[0],
+	m1.MergeFrom(&m2)
 
-	// 	"empty-string key was dropped during merge",
-	// )
-	// require.Equal(t,
-	// 	uint32(3),
-	// 	merged.Values[0],
+	// Correct behavior: "" must be included and summed.
+	require.Equal(t,
+		"",
+		*m1.Names[0].Load(),
 
-	// 	"merge did not accumulate empty-string key",
-	// )
+		"empty-string key was dropped during merge",
+	)
+	require.Equal(t,
+		uint32(3),
+		m1.Values[0].Load(),
+
+		"merge did not accumulate empty-string key",
+	)
+
+	var m3 MetaActive[string]
+
+	m3.MergeFrom(&m2)
+
+	require.Equal(t,
+		"",
+		*m3.Names[0].Load(),
+
+		"empty-string key was dropped during merge",
+	)
 }
